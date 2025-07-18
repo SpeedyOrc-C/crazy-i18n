3af97a6f08759a4eb14dd562aa3fe313847512c1
陈湛明 <chenzhanming04@foxmail.com>
2025-07-18 23:58:38 +0800

    Simplify

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-type Split<s extends string, sep extends string> =
-    s extends `${infer head}${sep}${infer tail}`
-        ? [head, ...Split<tail, sep>]
+type Split<s extends string> =
+    s extends `${infer head}.${infer tail}`
+        ? [head, ...Split<tail>]
         : [s]
 
 type Field<obj extends {[k: string]: any}, keys extends any[]> =
     keys extends [infer head, ...infer tail]
         ? head extends string
-            ? Field<Exclude<obj[head], undefined>, tail>
+            ? Field<NonNullable<obj[head]>, tail>
             : never
         : obj
 
-type LeafToArgs<leaf> =
+type LeafToArgs<leaf extends NonNullable<Leaf>> =
     leaf extends string
         ? []
-        : leaf extends (..._: [...infer args]) => string
-            ? args
-            : never
+        : Parameters<Exclude<leaf, string>>
 
 type GetArgs<obj extends {[k: string]: any}, key extends string> =
-    LeafToArgs<Field<obj, Split<key, ".">>>
+    LeafToArgs<Field<obj, Split<key>>>
 
 type ObjectOfDepth<depth, a> =
     depth extends 1 ? {[key: string]: a                      } :
@@ -30,7 +28,6 @@ type ObjectOfDepth<depth, a> =
     depth extends 7 ? {[key: string]: a | ObjectOfDepth<6, a>} :
     depth extends 8 ? {[key: string]: a | ObjectOfDepth<7, a>} :
     depth extends 9 ? {[key: string]: a | ObjectOfDepth<8, a>} :
-
                               never
 
 type Leaf
@@ -42,15 +39,10 @@ type LanguagePack = ObjectOfDepth<9, Leaf>
 
 type FlattenedKeysOf<obj> =
     obj extends LanguagePack
-        ?
-        _FlattenedKeysOf<obj, string & keyof obj>
+        ? _FlattenedKeysOf<obj, string & keyof obj>
         : never
 
-type _FlattenedKeysOf
-    < obj extends LanguagePack
-    , key extends string
-    >
-=
+type _FlattenedKeysOf<obj extends LanguagePack, key extends string> =
     key extends any
         ? obj[key] extends Leaf
             ? key
@@ -59,7 +51,7 @@ type _FlattenedKeysOf
 
 export function TranslatorFrom<LP extends LanguagePack>(languagePack: LP)
 {
-    function Translate<K extends FlattenedKeysOf<LP>>
+    return function <K extends FlattenedKeysOf<LP>>
     (key: K, ...args: GetArgs<LP, K>): string
     {
         const breadcrumbs = key.split(".")
@@ -89,14 +81,12 @@ export function TranslatorFrom<LP extends LanguagePack>(languagePack: LP)
             return key
         }
     }
-
-    return Translate
 }
 
 export function TranslatorWithFallbacks<LP extends LanguagePack>
 (...languagePacks: readonly LP[])
 {
-    function Translate<K extends FlattenedKeysOf<LP>>
+    return function <K extends FlattenedKeysOf<LP>>
     (key: K, ...args: GetArgs<LP, K>): string
     {
         const lps = new Array(...languagePacks)
@@ -140,6 +130,4 @@ export function TranslatorWithFallbacks<LP extends LanguagePack>
             breadcrumbs = key.split(".")
         }
     }
-
-    return Translate
 }
